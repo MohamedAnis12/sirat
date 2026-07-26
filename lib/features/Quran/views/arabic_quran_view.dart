c5cc9ce628681493338f998aef33673cb94d3259
anis <vidotyobegaming@gmail.com>
2026-07-27 02:37:32 +0300

    Refactor ArabicQuranView to StatefulWidget; implement PageView for page navigation and add bookmark action

--- lib/features/Quran/views/arabic_quran_view.dart
+++ lib/features/Quran/views/arabic_quran_view.dart
@@ -1,11 +1,67 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ArabicQuranView extends StatelessWidget {
+class ArabicQuranView extends StatefulWidget {
   const ArabicQuranView({super.key});
+  @override
+  State<ArabicQuranView> createState() => _QuranPageViewState();
+}
+class _QuranPageViewState extends State<ArabicQuranView> {
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose(); 
+    super.dispose();
+  }
+
+  void jumpToPageNumber(int pageNumber) {
+    if (pageNumber >= 1 && pageNumber <= 604) {
+      _pageController.jumpToPage(pageNumber - 1);
+    }
+  }
+
+  void animateToPageNumber(int pageNumber) {
+    if (pageNumber >= 1 && pageNumber <= 604) {
+      _pageController.animateToPage(
+        pageNumber - 1,
+        duration: const Duration(milliseconds: 400),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  String _getQuranPageAsset(int pageNumber) {
+    return 'assets/quran_image/$pageNumber.png';
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('القرآن الكريم'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.bookmark),
+            onPressed: () {
+              animateToPageNumber(1); 
+            },
+          ),
+        ],
+      ),
+      body: PageView.builder(
+        controller: _pageController, 
+        itemCount: 604,
+        reverse: true,
+        itemBuilder: (context, index) {
+          final int pageNumber = index + 1;
+
+          return Image.asset(
+            _getQuranPageAsset(pageNumber),
+            fit: BoxFit.fill,
+            alignment: AlignmentGeometry.center,
+          );
+        },
+      ),
+    );
   }
 }
