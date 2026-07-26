ec399b03087be40e4c5c4fe560fdcaded1953e54
anis <vidotyobegaming@gmail.com>
2026-07-26 15:22:52 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/sirat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sirat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Sirat());
+}
+
+class Sirat extends StatelessWidget {
+  const Sirat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(
+      
+    ));
+  }
+}
